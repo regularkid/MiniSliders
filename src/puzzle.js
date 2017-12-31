7edbe0a7622d6ece9ff6b2b2131737e99edfb92e
Bryan Perfetto <regularkid@gmail.com>
2017-12-31 10:18:09 -0800

    Puzzle rows actually move to correct position now as you move them with your mouse.

--- src/puzzle.js
+++ src/puzzle.js
@@ -10,7 +10,6 @@ var testPuzzle =
     [0,1,1,1,1,1,1,1,1,0],
     [0,0,1,1,1,1,1,1,0,0],
     [0,0,0,1,1,1,1,0,0,0],
-    
 ];
 
 var puzzle;
@@ -27,33 +26,33 @@ function createPuzzle()
     puzzle.numRows = puzzle.tileArray.length;
     puzzle.numCols = puzzle.tileArray[0].length;
     puzzle.tileSize = puzzle.numRows > puzzle.numCols ? desiredPuzzleSize / puzzle.numRows : desiredPuzzleSize / puzzle.numCols;
-    puzzle.puzzleWidth = (puzzle.numCols * puzzle.tileSize) + ((puzzle.numCols - 1) * spacing);
-    puzzle.puzzleHeight = (puzzle.numRows * puzzle.tileSize) + ((puzzle.numRows - 1) * spacing);
-    puzzle.x = (g.canvas.width - puzzle.puzzleWidth) / 2;
-    puzzle.y = (g.canvas.height - puzzle.puzzleHeight) / 2;
+    puzzle.xSize = (puzzle.numCols * puzzle.tileSize) + ((puzzle.numCols - 1) * spacing);
+    puzzle.ySize = (puzzle.numRows * puzzle.tileSize) + ((puzzle.numRows - 1) * spacing);
+    puzzle.x = (g.canvas.width - puzzle.xSize) / 2;
+    puzzle.y = (g.canvas.height - puzzle.ySize) / 2;
     puzzle.rows = [];
 
-    for (var row = 0; row < puzzle.numRows; row++)
+    for (var rowIndex = 0; rowIndex < puzzle.numRows; rowIndex++)
     {
         var puzzleRow = g.group();
         puzzleRow.x = 0;
-        puzzleRow.y = row * (puzzle.tileSize + spacing);
+        puzzleRow.y = rowIndex * (puzzle.tileSize + spacing);
         puzzleRow.isAttachedToMouse = false;
-        puzzleRow.row = row;
-        puzzleRow.rowHeight = puzzle.tileSize + spacing;
+        puzzleRow.rowIndex = rowIndex;
+        puzzleRow.ySize = puzzle.tileSize + spacing;
         puzzleRow.tiles = [];
-        puzzle.rows[row] = puzzleRow;
+        puzzle.rows[rowIndex] = puzzleRow;
         puzzle.addChild(puzzleRow);
 
-        for (var col = 0; col < puzzle.numCols; col++)
+        for (var colIndex = 0; colIndex < puzzle.numCols; colIndex++)
         {
-            var tile = puzzle.tileArray[row][col];
+            var tile = puzzle.tileArray[rowIndex][colIndex];
             var color = tile != 0 ? "black" : "white";
-            var x = col * (puzzle.tileSize + spacing);
+            var x =colIndex * (puzzle.tileSize + spacing);
             var y = 0;
 
             var tile = g.rectangle(puzzle.tileSize, puzzle.tileSize, color, "white", 0, x, y);
-            puzzleRow.tiles[col] = tile;
+            puzzleRow.tiles[colIndex] = tile;
             puzzleRow.addChild(tile);
         }
     }
@@ -70,21 +69,27 @@ function updatePuzzle()
         {
             puzzleRow.x = g.pointer.x + puzzleRow.mouseAttachOffsetX;
             puzzleRow.y = g.pointer.y + puzzleRow.mouseAttachOffsetY;
+            
+            var desiredRowIndex = getRowIndexFromYPos(puzzleRow.gy);
+            if (puzzleRow.rowIndex != desiredRowIndex)
+            {
+                setRowIndex(puzzleRow, desiredRowIndex);
+            }
         }
         else
         {
-            desiredPos = getDesiredRowPos(puzzleRow.row);
+            desiredPos = getDesiredRowPos(puzzleRow.rowIndex);
             puzzleRow.x += (desiredPos.x - puzzleRow.x) * 0.2;
             puzzleRow.y += (desiredPos.y - puzzleRow.y) * 0.2;
         }
     });
 }
 
-function getDesiredRowPos(row)
+function getDesiredRowPos(rowIndex)
 {
     pos = {};
     pos.x = 0;
-    pos.y = row * (puzzle.tileSize + spacing);
+    pos.y = rowIndex * (puzzle.tileSize + spacing);
 
     return pos;
 }
@@ -93,8 +98,8 @@ function onPuzzlePress()
 {
     puzzle.rows.forEach(function(puzzleRow)
     {
-        if (g.pointer.x >= puzzleRow.gx && g.pointer.x < puzzleRow.gx + puzzle.puzzleWidth &&
-            g.pointer.y >= puzzleRow.gy && g.pointer.y < puzzleRow.gy + puzzleRow.rowHeight)
+        if (g.pointer.x >= puzzleRow.gx && g.pointer.x < puzzleRow.gx + puzzle.xSize &&
+            g.pointer.y >= puzzleRow.gy && g.pointer.y < puzzleRow.gy + puzzleRow.ySize)
         {
             puzzleRow.mouseAttachOffsetX = (puzzleRow.gx - g.pointer.x) - puzzle.x;
             puzzleRow.mouseAttachOffsetY = (puzzleRow.gy - g.pointer.y) - puzzle.y;
@@ -114,4 +119,36 @@ function onPuzzleRelease()
     {
         puzzleRow.isAttachedToMouse = false;
     });
+}
+
+function getRowIndexFromYPos(y)
+{
+    var rowIndex = Math.floor(((y + puzzle.tileSize*0.5) - puzzle.y) / (puzzle.tileSize + spacing));
+    return Math.min(Math.max(rowIndex, 0), puzzle.numRows - 1);
+}
+
+function setRowIndex(puzzleRow, desiredRowIndex)
+{
+    if (puzzleRow.rowIndex > desiredRowIndex)
+    {
+        puzzle.rows.forEach(function(affectedPuzzleRow)
+        {
+            if (affectedPuzzleRow.rowIndex >= desiredRowIndex && affectedPuzzleRow.rowIndex < puzzleRow.rowIndex)
+            {
+                affectedPuzzleRow.rowIndex++;
+            }
+        });
+    }
+    else
+    {
+        puzzle.rows.forEach(function(affectedPuzzleRow)
+        {
+            if (affectedPuzzleRow.rowIndex <= desiredRowIndex && affectedPuzzleRow.rowIndex > puzzleRow.rowIndex)
+            {
+                affectedPuzzleRow.rowIndex--;
+            }
+        });
+    }
+
+    puzzleRow.rowIndex = desiredRowIndex;
 }
