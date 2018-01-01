eee48332425040ae84acb2a5d58aaf1842943094
Bryan Perfetto <regularkid@gmail.com>
2017-12-31 23:01:00 -0800

    Added logic to change the background color for each new puzzle.

--- src/main.js
+++ src/main.js
@@ -3,13 +3,29 @@ var assets =
     "fonts/upheavtt.ttf"
 ];
 
+var backgroundColorIdx = 0;
+var backgroundColors =
+[
+    "rgb(79, 128, 225)",    // Blue
+    "rgb(57, 185, 75)",     // Green
+    "rgb(151, 104, 209)",   // Purple
+    "rgb(243, 163, 70)",    // Orange
+    "rgb(255, 60, 60)",     // Red
+];
+
 var g = gaEngine(480, 480, onLoadComplete, assets);
 g.start();
 
 function onLoadComplete()
 {
-    g.canvas.style.backgroundColor = "rgb(79, 179, 225)";
+    g.canvas.style.backgroundColor = backgroundColors[backgroundColorIdx];
     g.interpolate = false;
 
     setState_Intro();
+}
+
+function changeBackgroundColor()
+{
+    backgroundColorIdx = (backgroundColorIdx + 1) % backgroundColors.length;
+    g.canvas.style.backgroundColor = backgroundColors[backgroundColorIdx];
 }
--- src/puzzle.js
+++ src/puzzle.js
@@ -144,6 +144,7 @@ function onPuzzlePress()
     {
         destroyPuzzle();
         createPuzzle();
+        changeBackgroundColor();        
     }
     else
     {
