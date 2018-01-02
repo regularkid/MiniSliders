844c2dd07a8fc1b0e532bd02f99f6ada56272f8f
Bryan Perfetto <regularkid@gmail.com>
2018-01-01 20:39:17 -0800

    Missed conversion to new setRenderLayer for a couple of text objects.

--- src/states/playing.js
+++ src/states/playing.js
@@ -46,7 +46,7 @@ function createInstructionsText()
     instructionsText[1] = g.text("the picture back together", "25px upheavtt", "white", g.canvas.width / 2, 445);
     instructionsText.forEach(function(text)
     {
-        text.layer = 2;
+        setRenderLayer(text, Layers.Top);
         centerTextObject(text);
         addShadowToObject(text);
     });
@@ -57,7 +57,7 @@ function createInstructionsText()
     controlsText[2] = g.text("S - Toggle Sound", "25px upheavtt", "white", 100, 530);
     controlsText.forEach(function(text)
     {
-        text.layer = 2;
+        setRenderLayer(text, Layers.Top);
         addShadowToObject(text);
     });
 }
